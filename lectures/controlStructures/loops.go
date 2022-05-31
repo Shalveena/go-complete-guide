6d02297a380e8564a2ac985dcdd9932b3e86399b
Shalveena Rohde <shalveena.s.prasad@gmail.com>
2022-06-01 07:12:13 +1000

    finish Onwards to Loops

--- lectures/controlStructures/loops.go
+++ lectures/controlStructures/loops.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+	"strconv"
 	"strings"
 )
 
@@ -14,18 +15,17 @@ var reader = bufio.NewReader(os.Stdin)
 
 func main() {
 
-	// If there is no error...
-	usersSelection, err := getUserInput()
-
+	userSelection, err := getUserInput()
+	// If there is an error, exit the program.
 	if err != nil {
 		fmt.Println("Invalid choice. Program exited.")
 		return
 	}
 
-	// ...depending on the user's choice call relevant function
-	switch usersSelection {
+	// If there is no error, depending on the user's choice call relevant function
+	switch userSelection {
 	case "1":
-		addUpToNumberX()
+		addUpToNumberX(int(chosenNumber))
 	case "2":
 		buildFactorial()
 	case "3":
@@ -52,12 +52,7 @@ func getUserInput() (string, error) {
 	}
 
 	// Clean user input
-	os := runtime.GOOS
-	if os == "windows" {
-		userInput = strings.Replace(userInput, "\r\n", "", -1)
-	} else {
-		userInput = strings.Replace(userInput, "\n", "", -1)
-	}
+	userInput = cleanUserInput(userInput)
 
 	// Check that user has only entered 1, 2, 3, or 4
 	if userInput == "1" || userInput == "2" || userInput == "3" || userInput == "4" {
@@ -68,7 +63,51 @@ func getUserInput() (string, error) {
 
 }
 
+func cleanUserInput(userInput string) string {
+	os := runtime.GOOS
+	if os == "windows" {
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+	} else {
+		userInput = strings.Replace(userInput, "\n", "", -1)
+	}
+
+	return userInput
+}
+
+func getInputNumber() (int, error) {
+	fmt.Println("Please enter a number")
+	chosenNumberString, err := reader.ReadString('\n')
+
+	if err != nil {
+		return 0, err
+	}
+
+	chosenNumberString = cleanUserInput(chosenNumberString)
+	chosenNumberInt, err := strconv.ParseInt(chosenNumberString, 0, 64)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return int(chosenNumberInt), nil
+}
+
 func addUpToNumberX() {
+
+	chosenNumber, err := getInputNumber()
+
+	if err != nil {
+		fmt.Println("Invalid input")
+		return
+	}
+	sum := 0
+
+	for i := 1; i <= chosenNumber; i++ {
+		sum += i
+		fmt.Println(sum)
+	}
+
+	return sum
 
 }
 
